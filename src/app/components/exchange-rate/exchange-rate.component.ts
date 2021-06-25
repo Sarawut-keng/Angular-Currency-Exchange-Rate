8883b6a9d5ea4e9984d772fa725c902c3bf81cbb
Sarawut <srwkk.8@gmail.com>
2021-06-25 19:52:33 +0700

    All done remain more css to make it perfect

--- src/app/components/exchange-rate/exchange-rate.component.ts
+++ src/app/components/exchange-rate/exchange-rate.component.ts
@@ -1,8 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+// Import from Angular
+
+import { Component, Input, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
-import { BackendService } from 'src/app/service/backend.service';
 
+// Import from service 
+
+import { BackendService } from 'src/app/service/backend.service';
+import { Convert } from 'src/app/service/backend.service';
 
 @Component({
   selector: 'app-exchange-rate',
@@ -11,25 +16,39 @@ import { BackendService } from 'src/app/service/backend.service';
 })
 export class ExchangeRateComponent implements OnInit {
 
-  form!: FormGroup;
-
-  countryCode: string[] = ['USD', 'THB', 'JPY'];
+  form!: FormGroup; 
+  accessKey!: string; // declare access key before call from service
+  currencyCode!: string[]; // declare currency code before calling from service
+  url!: string; // API
+  response!: any; // Response of API
+  toConvert!: Convert;
+  convertedValue!: number;
 
   constructor(private fb: FormBuilder, private backendService: BackendService, private httpClient: HttpClient) {
 
     this.form = this.fb.group({
-      currencyAmount: '',
-      currencyCode: ['', Validators.required],
-      convertedAmount: '',
-      convertedCode: ['', Validators.required]
+      fromCurrencyAmount: '',
+      fromCurrencyCode: ['', Validators.required],
+      toCurrencyAmount: '',
+      toCurrencyCode: ['', Validators.required]
     });
+
   }
 
   ngOnInit(): void {
+
+    this.accessKey = this.backendService.getAccessKey(); // calling access key from service
+    this.currencyCode = this.backendService.getCurrencyCode(); // calling access key from service
+    this.url = 'http://api.exchangeratesapi.io/v1/' + 'latest' + '?access_key=' + this.accessKey + '&format=1'; // url for calling API
+    this.httpClient.get(this.url).subscribe(result => {
+      this.response = result as any;
+    });
+
   }
 
   convertButton() {
-    alert(JSON.stringify(this.form.value));
+    this.toConvert = this.form.value;
+    this.convertedValue = this.toConvert.fromCurrencyAmount / this.response.rates[this.toConvert.fromCurrencyCode] * this.response.rates[this.toConvert.toCurrencyCode];
   };
 
 }
